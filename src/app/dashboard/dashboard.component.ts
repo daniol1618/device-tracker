80363952923474b7c261695cdaf4b9cabbecc7d2
daniel <daniol1618@gmail.com>
2026-01-18 11:05:13 -0500

    feat: service layer implemented to retrieve data from backend

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,36 +1,31 @@
-import { CommonModule } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
+
+import { DeviceService } from '../services/device.service';
+import { Device } from '../models/device';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [    
+  standalone: true,
+  imports: [
     CommonModule,
     MatCardModule
   ],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
 
-  devices: any[] = [];
-  statusMap = new Map<number, string>();
+  devices: Device[] = [];
 
-  constructor(private http: HttpClient) {}
+  constructor(private deviceService: DeviceService) {}
 
   ngOnInit(): void {
-    this.http.get<any>('http://localhost:8080/api/dashboard')
-      .subscribe(data => {
-        this.devices = data.devices;
-        data.statuses.forEach((s: any) =>
-          this.statusMap.set(s.id, s.name)
-        );
+    this.deviceService.getDashboardData()
+      .subscribe({
+        next: data => this.devices = data.devices,
+        error: err => console.error('Failed to load dashboard data', err)
       });
-  }
-
-  getStatusName(statusId: number): string {
-    console.log(this.statusMap);
-    return this.statusMap.get(statusId) ?? 'Unknown';
   }
 }
