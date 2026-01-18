9707b7afb5afec1cac9fcc58dbc5a920e41f52cd
daniel <daniol1618@gmail.com>
2026-01-18 10:58:39 -0500

    fix: assets moved as per version 21 requirement

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,36 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+
+@Component({
+  selector: 'app-dashboard',
+  imports: [    
+    CommonModule,
+    MatCardModule
+  ],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css'
+})
+export class DashboardComponent implements OnInit {
+
+  devices: any[] = [];
+  statusMap = new Map<number, string>();
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<any>('assets/devices.json')
+      .subscribe(data => {
+        this.devices = data.devices;
+        data.statuses.forEach((s: any) =>
+          this.statusMap.set(s.id, s.name)
+        );
+      });
+  }
+
+  getStatusName(statusId: number): string {
+    console.log(this.statusMap);
+    return this.statusMap.get(statusId) ?? 'Unknown';
+  }
+}
